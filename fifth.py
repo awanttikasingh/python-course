8d6089f7480ce3b741ee8a249e391aed9648d81c
awanttikasingh <92431564+awanttikasingh@users.noreply.github.com>
2023-03-21 22:16:39 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -70,9 +70,11 @@ items.sort(key=lambda item: item[1])
 print(items)
 
 
-prices = list(map(lambda item: item[1], items))
+# prices = list(map(lambda item: item[1], items))
+prices = [item[1] for item in items]
 print(prices)
 
 
-filtered = list(filter(lambda item: item[1] >= 10, items))
+# filtered = list(filter(lambda item: item[1] >= 10, items))
+filtered = [item for item in items if item[1] >= 10]
 print(filtered)
