8abe15a8b5c497c20dc19a2b0d4003e46df46ec9
awanttikasingh <92431564+awanttikasingh@users.noreply.github.com>
2023-03-21 22:54:54 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -87,3 +87,16 @@ list1 = [1, 2, 3]
 list2 = [10, 20, 30]
 
 print(list(zip("abd", list1, list2)))
+
+
+
+
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+print("redirect", browsing_session[-1])
