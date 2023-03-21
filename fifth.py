4d560054a2f32c4c073248c7430b0b8121d43eb4
awanttikasingh <92431564+awanttikasingh@users.noreply.github.com>
2023-03-21 21:14:25 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -28,3 +28,21 @@ letters = ["a", "b", "c", "d"]
 
 for index, letter in enumerate(letters):
     print(index, letter)
+
+    
+    
+    
+letters = ["a", "b", "c"]
+
+# add
+letters.append("d")
+letters.insert(0, "-")
+print(letters)
+
+
+# remove
+letters.pop(0)
+letters.remove("b")
+del letters[0:3]
+# letters.clear()
+print(letters)
