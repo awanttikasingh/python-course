96370e536e7196d09a8207e044de286bce300dcf
awanttikasingh <92431564+awanttikasingh@users.noreply.github.com>
2023-03-21 21:33:38 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -51,3 +51,20 @@ print(letters)
 print(letters.count("d"))
 if "d" in letters:
     print(letters.index("d"))
+
+    
+    
+    
+items = [
+    ("product1", 10),
+    ("product2", 9),
+    ("product3", 12),
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
