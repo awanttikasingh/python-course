cffc8fc6d5d124200e25b27b27342f1601501590
awanttikasingh <92431564+awanttikasingh@users.noreply.github.com>
2023-04-06 00:52:50 +0530

    Update seventh.py

--- seventh.py
+++ seventh.py
@@ -6,3 +6,28 @@ class Point:
 point = Point()
 print(type(point))
 print(isinstance(point, Point))
+
+
+
+
+
+
+class vehicle:
+    def __init__(self, color, type, terrain) -> None:
+        self.color = color
+        self.type = type
+        self.terrain = terrain
+
+    def __str__(self) -> str:
+        return (
+            "{ "
+            + f"color : {self.color}, type: {self.type}, terrain: {self.terrain}"
+            + " }"
+        )
+
+
+bus = vehicle("blue", "electric", "land")
+ship = vehicle("red", "fuel", "water")
+plane = vehicle("white", "fuel", "air")
+
+print(bus)
